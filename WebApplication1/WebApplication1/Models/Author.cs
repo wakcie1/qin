e84f84fe2dbff69f79a27007383ba1ee3dee29da
v-wanqin <v-wanqin@V-WANQIN-01>
2018-01-19 17:48:48 +0800

    small game

---
+++ WebApplication1/WebApplication1/Models/Author.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        //public ICollection<Book> Books { get; set; }
+    }
+}
---
+++ WebApplication1/WebApplication1/Scripts/page/TestIndex1.js
@@ -0,0 +1,69 @@
+﻿var app = angular.module('myApp', []);
+app.controller('myCtrl', function ($scope) {
+    $scope.size = 0;
+    $scope.grid = [];
+
+    function daAndXiao() {
+        var height = 76 * $scope.size;
+        var width = 76 * $scope.size;
+
+        $scope.Contain = {
+            "height": height + "px",
+            "width": width + "px"
+        }
+    }
+
+    $scope.$watch("size", function (v) {
+        v = parseInt(v) || 4;
+        $scope.grid = [];
+        for (var i = 0; i < v * v; i++) {
+            $scope.grid.push(0);
+        }
+        daAndXiao();
+    });
+
+    $scope.init = function () {
+        $scope.size = 2;
+        //$scope.grid = [0,0,0,0];
+        daAndXiao();
+    }
+    $scope.changeColor = function (index) {
+        var row = parseInt(index / ($scope.size));
+        var col = index % ($scope.size);
+        var dx = [0, 0, -1, 1, 0];
+        var dy = [1, -1, 0, 0, 0];
+        for (var d = 0; d < 5; d++) {
+            row += dx[d];
+            col += dy[d];
+            if (col>=0&&row>=0&&col < $scope.size && row < $scope.size) {
+                var temp = 1 - $scope.grid[row * $scope.size + col];
+                $scope.grid.splice(row * $scope.size + col,1,temp);
+            }
+            row -= dx[d], col -= dy[d]; 
+        }
+        var k=0;
+        for (var j = 0; j < $scope.size * $scope.size; j++) {
+            if ($scope.grid[j] > 0)++k;
+            if (k == ($scope.size * $scope.size)) {
+                alert("Sucess");
+                ++$scope.size;
+            }
+        }
+    }
+    $scope.up = function () {
+        if ($scope.size < 9) {
+            ++$scope.size;
+        } else {
+            alert("不能再升高了");
+        } 
+    }
+    $scope.down = function () {
+        if ($scope.size > 2) {
+            --$scope.size;
+        } else {
+            alert("不能再降低了");
+        }
+        
+    }
+
+});
